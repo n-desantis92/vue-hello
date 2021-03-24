c47b91858e00d020bf0555986dce08182633f8bf
n-desantis92 <dsnwebdev@gmail.com>
2021-03-24 15:33:57 +0100

    aggiunte cartelle e uso vuejs

---
+++ script/script.js
@@ -0,0 +1,21 @@
+/*Descrizione:
+Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+Bonus:
+Aggiungere alla pagina un’immagine, presa anch’essa da un data.*/
+
+
+// passo 1 dichiarare dove utilizzare VUEjs
+
+var app = new Vue({
+    el: "#app",
+
+    data: {
+      messaggio: "ciao",
+      nome: "",
+      cognome: "",
+      immagine: "img/immagini-felicità.jpg",
+      classi:[
+
+      ],
+    }
+  });
